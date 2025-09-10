9772d746d343292e1c33d99af35f1c26274b5bca
Sagar <sagar@Sagars-MacBook-Air.local>
2025-09-10 22:52:20 +0530

    Just a site for data Entry

---
+++ index.js
@@ -0,0 +1,41 @@
+
+document.getElementById("dob").addEventListener("change", function () {
+    const dob = new Date(this.value);
+    const today = new Date();
+    let age = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+
+    // adjust if birthday hasn't come yet this year
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+
+    document.getElementById("age").value = age >= 0 ? age : "";
+});
+
+const form = document.getElementById("employeeForm");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+    const res = await fetch("http://localhost:5000/api/employees", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    if (res.ok) {
+      alert("Form submitted & saved in MongoDB!");
+      form.reset();
+    } else {
+      alert("Error saving data!");
+    }
+  } catch (err) {
+    console.error(err);
+    alert("Server error!");
+  }
+});
+
---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+console.log(process.env.MONGODB_URI);
+
+const client = new MongoClient(process.env.MONGODB_URI);
+
+async function run() {
+    await client.connect();
+    const db = client.db("dataentry");
+    const employees = db.collection("employees1");
+
+    // POST route for saving form data
+    app.post("/api/employees", async (req, res) => {
+        try {
+            await employees.insertOne(req.body);
+            res.status(200).json({ message: "Saved successfully" });
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Database error" });
+        }
+    });
+    app.get("/api/employees", async (req, res) => {
+        try {
+            const allEmployees = await employees.find({}).toArray();
+            res.status(200).json(allEmployees);
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: "Database error" });
+        }
+    });
+
+
+    app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+}
+run();
